f7889f5bd5fab21a24b47d97dea342036b23aa0e
Anubhuti Anand <anubhuti.anand@modernizingmedicine.com>
2023-03-15 15:33:13 -0400

    Auto trigger

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Running my first Jenkins job");
+        System.out.println("Auto triggering");
     }
 }
