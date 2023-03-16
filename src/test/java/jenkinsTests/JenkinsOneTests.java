6d233b60db1767e8683b57bf72223ad0af4a2cf1
Anubhuti Anand <anubhuti.anand@modernizingmedicine.com>
2023-03-16 05:05:50 -0400

    Pipeline code test

---
+++ src/test/java/jenkinsTests/JenkinsOneTests.java
@@ -0,0 +1,26 @@
+package jenkinsTests;
+
+import org.testng.annotations.Test;
+
+public class JenkinsOneTests {
+
+
+    @Test
+    public void FirstTest() {
+
+        System.out.println("Running jenkins pipeline");
+    }
+
+    @Test
+    public void SecondTest() {
+        System.out.println("Running jenkins pipeline example 2");
+
+    }
+
+    @Test
+
+    public void ThirdTest() {
+        System.out.println("Running jenkins pipeline example 3");
+
+    }
+}
